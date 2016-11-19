4f4983bddd180094be4619138c7ea34912b415c3
Abhishek Tiwari <abhishekptrt@gmail.com>
2016-11-19 21:06:04 +0530

    Adding footer header

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">			
+			<!-- Footer -->
+				<footer>
+					<p><img src="images/twitter.png" alt="twitter" />&nbsp;<img src="images/facebook.png" alt="facebook" />&nbsp;<img src="images/rss.png" alt="rss" /></p> 
+					<!--<p><a href="index.html">Home</a> | <a href="examples.html">Examples</a> | <a href="apage.html">A Page</a> | <a href="anotherpage.html">Another Page</a> | <a href="contact.php">Contact Us</a></p>-->
+					<p>Copyright &copy; ayodhyadham.com </p>
+				</footer>		
+			</div>		
+			
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php include "header.php"; ?>
+		<div id="site_content">
+			<div class="container">			
+				
+			<!-- Features -->
+				<div class="row">
+					<section class="6u">
+						<h1>Welcome to ayodhyadham.com</h1>
+						<p>ayodhyadham.com पर  हिन्दू रीति  से  पूजन संबंधी  समस्त जानकारी  उपलब्ध  करने का प्रयाश है। इस वेबसाइट के माध्यम से पुरोहित , पूजन विधि , पूजन सामग्री  गुडवत्ता के साथ उपलब्ध कराया जाता है । 
+
+आप पूजन विधि या पूजन सम्बंधित कोई भी जानकारी प्राप्त कर सकते है। 
+</p>
+
+					</section>
+					<section class="6u">
+						<a href="#" class="image" ><img src="images/ayo_pujan.jpg" alt="image"/></a>
+					</section>					
+				</div>
+				<div class="row" style="display:none;">	
+					<section class="4u">
+						<div class="box">						
+							<h3>Template Details</h3>
+							<p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS3</strong>. You can view more free CSS3 web templates <a href="http://www.css3templates.co.uk">here</a>.</p>
+							<p>This template is a fully documented 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design. There is also a working PHP contact form on the contact page.</p>
+						</div>
+					</section>		
+					<section class="4u">
+						<div class="box">
+							<h3>Browser Compatibility</h3>
+							<p>This template has been tested in the following browsers:</p>
+							<ul>
+								<li>Internet Explorer 9</li>
+								<li>FireFox 24</li>
+								<li>Google Chrome 31</li>
+							</ul>
+						</div>
+					</section>						
+					<section class="4u">
+						<div class="box">
+							<section class="12u">
+								<h3>Latest News</h3>
+								<h4>New Website Launched</h4>
+								<h5>August 1st, 2014</h5>
+								<p>2014 sees the redesign of our website. <a href="#">Read more</a></p>
+							</section>
+							<section class="12u">
+								<h3>Useful Links</h3>
+								<ul>
+									<li><a href="#">First Link</a></li>
+									<li><a href="#">Another Link</a></li>
+									<li><a href="#">And Another</a></li>
+									<li><a href="#">Last One</a></li>
+								</ul>
+							</section>
+						</div>
+					</section>
+					
+				</div>
+			</div>
+        </div>		
+<?php include "footer.php ";?>		
+			
